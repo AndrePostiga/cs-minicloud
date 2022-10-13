d447d9cd1052099331bac2344e203d835bdd7ee0
Andre Felipe Brasil Postiga <andre.postiga@mundipagg.com>
2022-10-12 21:38:03 -0300

    sql script on init

---
+++ sql-scripts/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO minicloud.cpus (architecture, cache, clockFrequency, cores)
+values ('X86',2048,5.0,4);
+
+INSERT INTO minicloud.cpus (architecture, cache, clockFrequency, cores)
+values ('ARM',1048,2.0,8);
---
+++ sql-scripts/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS minicloud;
+CREATE DATABASE minicloud;
+
+create table minicloud.cpus (
+    id bigint not null auto_increment,
+    architecture varchar(255) not null,
+    cache integer not null,
+    clockFrequency double precision not null,
+    cores integer not null,
+    primary key (id)
+) engine=InnoDB
+CHARACTER SET utf8mb4;
