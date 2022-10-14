be57ee2da51560c0e343e7ff0820a05b47164245
Andre Postiga Stone <andre.postiga@pagar.me>
2022-10-13 21:26:50 -0300

    fix data.sql file

--- sql-scripts/data.sql
+++ sql-scripts/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO minicloud.cpus (architecture, cache, clockFrequency, cores)
+INSERT INTO minicloud.cpus (architecture, cache, clock_frequency, cores)
 values ('X86',2048,5.0,4);
 
-INSERT INTO minicloud.cpus (architecture, cache, clockFrequency, cores)
+INSERT INTO minicloud.cpus (architecture, cache, clock_frequency, cores)
 values ('ARM',1048,2.0,8);
