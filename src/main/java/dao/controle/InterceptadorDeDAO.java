1bdf9cdb9b82a8ce491961315ae4bde1af5996dc
Andre Felipe Brasil Postiga <andre.postiga@mundipagg.com>
2022-11-07 22:17:56 -0300

    refatoração do trabalho

--- src/main/java/dao/controle/InterceptadorDeDAO.java
+++ src/main/java/dao/controle/InterceptadorDeDAO.java
@@ -11,30 +11,30 @@ import java.lang.reflect.Method;
 public class InterceptadorDeDAO implements MethodInterceptor {
 
     private Class<?> classeInterceptada;
+    private Field campo;
 
     public InterceptadorDeDAO(Class<?> classe) {
         this.classeInterceptada = classe;
+        Field[] campos = this.classeInterceptada.getDeclaredFields();
+        for (Field campo : campos) {
+            if (campo.isAnnotationPresent(PersistenceContent.class)) {
+                this.campo = campo;
+            }
+        }
     }
 
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
         try {
             System.out.println(">>>>>>>>> Começou Interceptação de método <<<<<<<<<");
-            Field[] campos = this.classeInterceptada.getDeclaredFields();
-            for (Field campo : campos) {
-                if (campo.isAnnotationPresent(PersistenceContent.class)) {
-                    campo.setAccessible(true);
-                    try {
-                        campo.set(o, JPAUtil.getEntityManager());
-                    } catch (IllegalArgumentException | IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-
+            campo.setAccessible(true);
+            campo.set(o, JPAUtil.getEntityManager());
             Object obj = methodProxy.invokeSuper(o, objects);
             System.out.println(">>>>>>>>> Terminou Interceptação de método <<<<<<<<<");
             return obj;
+        }
+        catch (IllegalArgumentException | IllegalAccessException e) {
+            throw new RuntimeException(e);
         }
         catch (Exception e) {
             throw e;
